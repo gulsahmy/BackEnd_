78b2dd47df546ba530ab5d9fbd8939790c542e48
gulsah <gmutluayyigit@gmail.com>
2024-07-13 18:33:00 +0300

    get - set

--- OOP_2/oop2.js
+++ OOP_2/oop2.js
@@ -65,7 +65,7 @@ const Ford = new Car('Ford', 'Mustang', 1967)
 
 const Mercedes = new Car('Mercedes', 'CLK300', 2020)
 console.log(Mercedes)
-/* ----------------------------------------------------- */
+/* ----------------------------------------------------- *
 //* INHERITANCE = MirasAlma / Başka bir classın tüm property/methodlarını devralma (parent-child ilişkisi kurulur)
 //* SUPER = Parent Class, THIS = Child (Current) Class 
 
@@ -123,12 +123,159 @@ const BoshClimate = new Accessory('Bosh Climate', ...Object.values(Mercedes))
 console.log(BoshClimate)
 
 
-
-
 // Truck extends Vehicle{...}
 
+/* ----------------------------------------------------- *
+//? Polymorphism: Miras aldığımız sınıfın özellik/methodlarını yeniden yazabilme.
+//? - Override: Üst metodla aynı isim ve yapıda yeni bir metod yazma. (ezme / iptal etme / önceliğini alma)
+//? - Overload: Üst metodla aynı isimde ama farklı yapıda (farklı adet/tip) yeni method oluşturma. (aynı anda ikisi de aktif) (JS desteklemez)
+
+class Vehicle {
+
+    vehicleIsActive = false
+
+    constructor(vehicleType) {
+        this.vehicleType = vehicleType
+    }
+
+    getDetails() {
+        console.log('Vehicle Class getDetails()')
+        return this
+    }
+}
+
+class Car extends Vehicle {
+
+    isRunning = false    
+
+    constructor(brand, model, year, vehicleType){
+        super(vehicleType)
+        this.brand = brand
+        this.model = model
+        this.year = year
+    }
+
+    runEngine() {
+        this.isRunning = true
+        console.log('Engine started')
+        return this.isRunning
+    }
+
+    // Override: Parent classtaki methodu aynı isimle tekrar tanımlama.
+    //Override işlemi parent classtaki methodu ezer(önceliği alır)
+
+    getDetails(){
+        console.log('Car Class getDetails()')
+        super.getDetails()
+    }
+
+    //! Class içine sadece property ve method yazabiliriz.
+
+    //? Overload: Üstteki methodun aynı isim ama farklı parametre adet/tip ile yeniden tanımlanması.
+    //? JS Desteklemez. TypeScript destekler.
+    //? Çalışma prensibi: Çağrıldığı zaman parametreye göre ilgili method çalışır.
+    // getDetails(parametre1, parameter2) {
+    //     return this
+    // }
+
+}
+
+const Ford = new Car('Ford', 'Mustang', 1967, 'Car')
+console.log(Ford)
+console.log(Ford.getDetails())
+/* ----------------------------------------------------------------------------------- *
+
+//? Access Modifiers:
+//? - PUBLIC: Genel erişime açık. (Parent: Yes, Child: Yes, Instance: Yes)
+//? - _PROTECTED: Sadece tanımlı olduğu class ve Inherit edilen child-class erişebilir. (Parent: Yes, Child: Yes, Instance: No) (JS Desteklemez.)
+//? - #PRIVATE: Sadece tanımlı olduğu class içinde erişim var. (Parent: Yes, Child: No, Instance: No)
+
+
+class Vehicle {
+
+    #vehicleIsActive = false
+
+    constructor(vehicleType) {
+        this.vehicleType = vehicleType
+    }
+
+    getDetails() {
+        console.log('Vehicle Class getDetails()')
+        return this
+    }
+}
+
+class Car extends Vehicle {
+
+    isRunning = false    
+
+    constructor(brand, model, year, vehicleType){
+        super(vehicleType)
+        this.brand = brand
+        this.model = model
+        this.year = year
+    }
+
+    runEngine() {
+        this.isRunning = true
+        console.log('Engine started')
+        console.log(this.vehicleIsActive)
+        return this.isRunning
+    }
+}    
+const Ford = new Car('Ford', 'Mustang', 1967, 'Car')
+console.log(Ford)
+console.log(Ford.runEngine())
+console.log(Ford.vehicleIsActive)
+/* ----------------------------------------------------- *
+// //* Piyasa Standartı İsimlendirme:
+
+// const BUYUK_HARF_ISIMLENDIRME = Bu bir constant değişkendir. Developer olarak bunu silme, değiştirirken dikkatli ol.
+// const _alttanTireIleBaslayan = BU bir proctected değişkendir. Developer olarak buna dokunma (erişme bile)
+
+
 /* ----------------------------------------------------- */
-/* ----------------------------------------------------- */
-/* ----------------------------------------------------- */
-/* ----------------------------------------------------- */
+
+//? GETTER & SETTER METHODS: Görevi veri getirme (getter) ve veri güncelleme (setter) olan metodlardır.
+//? "STATIC" KEYWORD: Class'dan direkt erişim. (Instance erişemez.)
+
+
+class Car {
+
+    isRunning = false  
+    #price = 99 
+
+    constructor(brand, model, year){
+        this.brand = brand
+        this.model = model
+        this.year = year
+    }
+
+    runEngine() {
+        this.isRunning = true
+        console.log('Engine started')
+        return this.isRunning
+    }
+
+    get getPrice(){
+        return this.#price
+    }
+
+    set setPrice(price) {
+        this.#price = price
+        return 'Fiyat güncellendi.'
+    }
+}    
+
+const Ford = new Car('Ford', 'Mustang', 1967)
+console.log(Ford)
+console.log(Ford.price)
+console.log(Ford.getPrice)
+// Ford.setPrice(8000)
+Ford.setPrice = 8000
+console.log(Ford.getPrice)
+
+
+
+
 /* ----------------------------------------------------- */
