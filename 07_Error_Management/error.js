6243886670148cfd083dd49127e799cc1f40130e
gulsah <gmutluayyigit@gmail.com>
2024-07-15 23:15:28 +0300

    errorHandler

--- 07_Error_Management/error.js
+++ 07_Error_Management/error.js
@@ -23,16 +23,55 @@ app.get('/user/:id', function (req, res) {
         // })
 
         if (isNaN(req.params.id)){
+
+                res.errorStatusCode = 400 // errorHandler a statusCode gönderdik.
+
                 throw new Error('ID parametresi sayı olmak zorundadır.')
         } else {
                 res.send('ID doğru.')
         }
 })
+/* ---------------------------------------------------------------- *
 
+app.get('/user/:id', function(req, res){
+        try {
+                if(isNaN(req.params.id)){
+                        throw new Error('ID parametresi sayı olmak zorundadır.')
+                } else {
+                       res.status(200).send({
+                        error: false,
+                        message: 'ID doğru. Herşey yolunda gidiyor.'
+                       })
 
+                }
+        } catch (err) {
+                // Hata oluşursa catch() çalışır ve hata err parametresi ile gönderilir.
 
+                res.status(400).send({
+                        error: true,
+                        message: err.message
+                })
+        }
+})
 /* ---------------------------------------------------------------- */
-/* ---------------------------------------------------------------- */
+// ErrorHandler 4 parametreli olmak zorunda. Hata yakalayıcı parametre 1. parametredir.
+// ErrorHandler en sonda yer almalı
+
+const errorHandler = (error, req, res, next) => {
+
+        const statusCode = res?.errorStatusCode || 500
+
+        res.status(statusCode).send({
+                error: true,
+                message: error.message
+        })
+}
+app.use(errorHandler)
+
+
+
+
+
 /* ---------------------------------------------------------------- */
 /* ---------------------------------------------------------------- */
 /* ---------------------------------------------------------------- */
