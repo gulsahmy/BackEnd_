6d2bfc9db868323cdd67c2145a5d697b35dbb6a2
gulsah <gmutluayyigit@gmail.com>
2024-07-27 12:53:12 +0300

    start

--- 15_PersonnelApi/src/models/department.model.js
+++ 15_PersonnelApi/src/models/department.model.js
@@ -1,21 +1,18 @@
-"use strict";
+"use strict"
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const { mongoose } = require("../configs/dbConnection");
-
+const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
-const DepartmentSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: true,
-      unique: true,
-    },
-  },
-  { collection: "departments", timestamps: true },
-);
 
-module.exports = mongoose.model("Department", DepartmentSchema);
+const DepartmentSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    }
 
+}, {collection:"department", timestamps:true})
+
+module.exports=mongoose.model("Department", DepartmentSchema)
